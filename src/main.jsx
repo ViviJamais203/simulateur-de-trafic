09ac3ae5c17a7f187ba0f542b5be99744c339d0d
Margotin Virgile <virgile.margotin@rpn.ch>
2026-05-05 10:51:28 +0200

    Changed basename in the router

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './style.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/simulateur-de-trafic">
     <App />
   </BrowserRouter>,
 )
